f7eb689940808418381c62bd238c674424fe5921
Steven P. Kennedy <umbrylax@gmail.com>
2019-11-06 10:54:17 -0700

    game view set up

---
+++ src/views/Game.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="game"></div>
+</template>
+
+
+<script>
+export default {
+  name: "game",
+  data() {
+    return {};
+  },
+  computed: {
+    hero() {
+      return this.$store.state.hero;
+    },
+    inventory() {
+      return this.$store.state.inventory;
+    },
+    enemies() {
+      return this.$store.state.activeEnemies;
+    }
+  },
+  methods: {
+    heroDamaged() {},
+    enemyDamaged(enemy) {},
+    itemUsed(item) {},
+    itemEquipped(item) {}
+  },
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
